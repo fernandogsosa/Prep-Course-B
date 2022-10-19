d94aa5e8fd20ed01744c14436dec2befb765a9ff
fernandogsosa <peelusosa@gmail.com>
2022-10-19 19:04:30 -0300

    dos mas y ya

--- 02-JS-II/homework/homework.js
+++ 02-JS-II/homework/homework.js
@@ -133,20 +133,20 @@ else {
   function redondearNumero(num) {
     // Redondea "num" al entero más próximo y devuélvelo
     // Tu código:
-    return math.round(num)
+    return Math.round(num)
   }
   
   function redondearHaciaArriba(num) {
     // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
     // Tu código:
-    return math.ceil(num)
+    return Math.ceil(num)
   }
   
   function numeroRandom() {
     //Generar un número al azar entre 0 y 1 y devolverlo
     //Pista: investigá qué hace el método Math.random()
     // Tu código:
-    return math.random()
+    return Math.random()
   }
   
   function esPositivo(numero) {
